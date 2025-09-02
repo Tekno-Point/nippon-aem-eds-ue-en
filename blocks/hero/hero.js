1f47b52abd343b0bebd23e9f1372f8df3b05339b
Krishna Mulik <krishna.mulik@teknopoint.local>
2025-09-02 14:45:48 +0530

    change in hero model

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,28 +1,41 @@
-import { div, video, source } from "../../scripts/dom-helper.js";
+import { div, video, source } from '../../scripts/dom-helper.js';
+
+function getVideos(url) {
+  return video(
+    { autoplay: true, muted: true, playsinline: true },
+    source({ src: url, type: 'video/mp4' }, 'Your browser does not support the video tag.'),
+  );
+}
+
+function getImg(isDesktop, deskImg, mobImg) {
+  if (isDesktop.matches) {
+    return deskImg;
+  }
+
+  if (mobImg.querySelector('img')) {
+    return mobImg;
+  }
+
+  return deskImg;
+}
 
 export default function decorate(block) {
   const blockChildrens = [...block.children];
   // media query match that indicates mobile/tablet width
   const isDesktop = window.matchMedia('(min-width: 900px)');
   const isVideo = blockChildrens[0].textContent.trim() === 'video';
-  const richText = blockChildrens[5];
+  const [richText] = blockChildrens.slice(5);
 
   if (isVideo) {
-    const [deskVid, mobVid] = blockChildrens.slice(3, 5).map(c => c.querySelector('a').getAttribute('href'));
-    const videoUrl = isDesktop.matches ? deskVid : mobVid ? mobVid : deskVid;
-    const video = getVideos(videoUrl);
+    const [deskVid, mobVid] = blockChildrens.slice(3, 5).map((c) => c.querySelector('a').getAttribute('href'));
+    const videoUrl = isDesktop.matches ? deskVid : mobVid || deskVid;
+    const videoEl = getVideos(videoUrl);
     block.textContent = '';
-    block.append(video, richText);
+    block.append(videoEl, richText);
   } else {
     const [deskImg, mobImg] = blockChildrens.slice(1, 3);
-    const img = isDesktop.matches ? deskImg : mobImg.querySelector('img') ? mobImg : deskImg;
+    const img = getImg(isDesktop, deskImg, mobImg);
     block.textContent = '';
     block.append(img, richText);
   }
-}
-
-function getVideos(url) {
-  return video({"autoplay": true,  "muted": true, "playsinline": true},
-    source({ src: url, type: 'video/mp4' }, "Your browser does not support the video tag.")
-  );
 }
