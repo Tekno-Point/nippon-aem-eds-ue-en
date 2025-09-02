aa0584d97266490afec8e729be9b254bd0133444
Krishna Mulik <krishna.mulik@teknopoint.local>
2025-09-02 11:20:55 +0530

    changes in video atrrs

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -5,23 +5,24 @@ export default function decorate(block) {
   // media query match that indicates mobile/tablet width
   const isDesktop = window.matchMedia('(min-width: 900px)');
   const isVideo = blockChildrens[0].textContent.trim() === 'video';
+  const richText = blockChildrens[5];
 
   if (isVideo) {
     const [deskVid, mobVid] = blockChildrens.slice(3, 5).map(c => c.querySelector('a').getAttribute('href'));
     const videoUrl = isDesktop.matches ? deskVid : mobVid ? mobVid : deskVid;
     const video = getVideos(videoUrl);
-    block.append(video);
-    blockChildrens.slice(0, 5).forEach(c => block.removeChild(c));
+    block.textContent = '';
+    block.append(video, richText);
   } else {
     const [deskImg, mobImg] = blockChildrens.slice(1, 3);
     const img = isDesktop.matches ? deskImg : mobImg.querySelector('img') ? mobImg : deskImg;
-    blockChildrens.slice(0, 5).forEach(c => block.removeChild(c));
-    block.append(img);
+    block.textContent = '';
+    block.append(img, richText);
   }
 }
 
 function getVideos(url) {
-  return video({ autoplay: true, playsinline: true, loop: true, muted: true, controls: true },
+  return video({"autoplay": true,  "muted": true, "loop": true, "playsinline": true},
     source({ src: url, type: 'video/mp4' }, "Your browser does not support the video tag.")
   );
 }
