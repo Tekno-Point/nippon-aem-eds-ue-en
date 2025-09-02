2331646c747da6cff0c2043156d489a997ffd49e
Krishna Mulik <krishna.mulik@teknopoint.local>
2025-09-02 11:07:14 +0530

    hero-block js

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,17 +1,27 @@
+import { div, video, source } from "../../scripts/dom-helper.js";
 
 export default function decorate(block) {
-  const imagesContainer = block.querySelector('[name="imagesContainer"]');
-  const videosContainer = block.querySelector('[name="videosContainer"]');
-  const images = imagesContainer.querySelectorAll('img');
-  const videos = videosContainer.querySelectorAll('video');
-  if (images.length > 0) {
-    images.forEach((image) => {
-      image.closest('div').classList.add('image');
-    });
+  const blockChildrens = [...block.children];
+  // media query match that indicates mobile/tablet width
+  const isDesktop = window.matchMedia('(min-width: 900px)');
+  const isVideo = blockChildrens[0].textContent.trim() === 'video';
+
+  if (isVideo) {
+    const [deskVid, mobVid] = blockChildrens.slice(3, 5).map(c => c.querySelector('a').getAttribute('href'));
+    const videoUrl = isDesktop.matches ? deskVid : mobVid ? mobVid : deskVid;
+    const video = getVideos(videoUrl);
+    block.append(video);
+    blockChildrens.slice(0, 5).forEach(c => block.removeChild(c));
+  } else {
+    const [deskImg, mobImg] = blockChildrens.slice(1, 3);
+    const img = isDesktop.matches ? deskImg : mobImg.querySelector('img') ? mobImg : deskImg;
+    blockChildrens.slice(0, 5).forEach(c => block.removeChild(c));
+    block.append(img);
   }
-  if (videos.length > 0) {
-    videos.forEach((video) => {
-      video.closest('div').classList.add('video');
-    });
-  }
+}
+
+function getVideos(url) {
+  return video({ autoplay: true, playsinline: true, loop: true, muted: true, controls: true },
+    source({ src: url, type: 'video/mp4' }, "Your browser does not support the video tag.")
+  );
 }
--- scripts/dom-helper.js
+++ scripts/dom-helper.js
@@ -40,34 +40,34 @@
  * @returns {Element} The constructred DOM Element
  */
 export function domEl(tag, ...items) {
-    const element = document.createElement(tag);
+  const element = document.createElement(tag);
 
-    if (!items || items.length === 0) return element;
+  if (!items || items.length === 0) return element;
 
-    if (!(items[0] instanceof Element || items[0] instanceof HTMLElement) && typeof items[0] === 'object') {
-        const [attributes, ...rest] = items;
-        items = rest;
+  if (!(items[0] instanceof Element || items[0] instanceof HTMLElement) && typeof items[0] === 'object') {
+    const [attributes, ...rest] = items;
+    items = rest;
 
-        Object.entries(attributes).forEach(([key, value]) => {
-            if (typeof value === 'boolean') {
-                if (value) element.setAttribute(key, '');
-                else element.removeAttribute(key);
-            } else if (!key.toLowerCase().startsWith('on')) {
-                element.setAttribute(key, Array.isArray(value) ? value.join(' ') : value);
-            } else {
-                element.addEventListener(key.substring(2).toLowerCase(), value);
-            }
-        });
-    }
-
-    items.forEach((item) => {
-        item = item instanceof Element || item instanceof HTMLElement
-            ? item
-            : document.createTextNode(item);
-        element.appendChild(item);
+    Object.entries(attributes).forEach(([key, value]) => {
+      if (typeof value === 'boolean') {
+        if (value) element.setAttribute(key, '');
+        else element.removeAttribute(key);
+      } else if (!key.toLowerCase().startsWith('on')) {
+        element.setAttribute(key, Array.isArray(value) ? value.join(' ') : value);
+      } else {
+        element.addEventListener(key.substring(2).toLowerCase(), value);
+      }
     });
+  }
 
-    return element;
+  items.forEach((item) => {
+    item = item instanceof Element || item instanceof HTMLElement
+      ? item
+      : document.createTextNode(item);
+    element.appendChild(item);
+  });
+
+  return element;
 }
 
 /*
@@ -100,3 +100,5 @@ export function article(...items) { return domEl('article', ...items); }
 export function strong(...items) { return domEl('strong', ...items); }
 export function select(...items) { return domEl('select', ...items); }
 export function option(...items) { return domEl('option', ...items); }
+export function video(...items) { return domEl('video', ...items); }
+export function source(...items) { return domEl('source', ...items); }
